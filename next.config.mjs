78aa6134cf18f5862ace6e2423f55046618c58eb
Kohei Morita <moritakouhei@graffer.jp>
2024-12-29 18:04:30 +0900

    docs: add config for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 import nextra from 'nextra'
  
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+}
+
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx'
 })
- 
-export default withNextra()
+
+export default withNextra(nextConfig)
